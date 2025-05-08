a99c35bd4e55bfe946dcd6b3fd7b8ce1cd5b5d44
Huni0128 <tlacogns@gmail.com>
2025-05-08 10:34:12 +0900

    edit cartographer_params.lua

--- src/pinky_cartographer/params/cartographer_params.lua
+++ src/pinky_cartographer/params/cartographer_params.lua
@@ -47,10 +47,12 @@ MAP_BUILDER.use_trajectory_builder_2d = true
 
 TRAJECTORY_BUILDER_2D.min_range = 0.12
 TRAJECTORY_BUILDER_2D.max_range = 3.5
-TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
+TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.0
 TRAJECTORY_BUILDER_2D.use_imu_data = true
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true 
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
+TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 5.0         -- 장애물 유지시간 늘림
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 120                -- Submap 갱신 간격 증가
 
 POSE_GRAPH.constraint_builder.min_score = 0.65
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
